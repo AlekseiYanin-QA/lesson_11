5f5f904d7ceaf80cf2100ecfa1b8e0d0f2393a7c
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-10-10 08:18:25 +0300

    Дополнение к классу LetterCountInWords

--- LetterCountInWords.java
+++ LetterCountInWords.java
@@ -1,11 +1,16 @@
 public class LetterCountInWords {
     public static void countEsInWords(String input) {
-        String[] words = input.split("\\s+");
+        // Удаляем знаки препинания с помощью регулярного выражения
+        String cleanedInput = input.replaceAll("[^\\p{L}\\s]", "");
+        String[] words = cleanedInput.split("\\s+");
+
         for (String word : words) {
             int countE = 0;
+            // Приводим слово к нижнему регистру
+            String lowerCaseWord = word.toLowerCase();
 
-            for (int i = 0; i < word.length(); i++) {
-                if (word.charAt(i) == 'е' || word.charAt(i) == 'Е') {
+            for (int i = 0; i < lowerCaseWord.length(); i++) {
+                if (lowerCaseWord.charAt(i) == 'е') {
                     countE++;
                 }
             }
